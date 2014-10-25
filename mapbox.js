b110bc5c32a3599eaa7e296e0007cb8e7d90d6b0
Kyle Wilson <bluyam@tamu.edu>
2014-10-25 13:51:36 -0500

    changed get id

--- mapbox.js
+++ mapbox.js
@@ -12,7 +12,7 @@ function initialize() {
 
   // Create the search box and link it to the UI element.
   var input = /** @type {HTMLInputElement} */(
-      document.getElementById('pac-input'));
+      document.getElementById('StartingLocation'));
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   var searchBox = new google.maps.places.SearchBox(
