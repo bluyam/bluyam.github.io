3accb305427381f38bbaf9d5afa6924564535498
Kyle Wilson <bluyam@tamu.edu>
2014-11-08 21:18:00 -0600

    image animation 3

--- caveat/style.js
+++ caveat/style.js
@@ -1,5 +1,6 @@
 var main = function() {
-	setTimeout("$('#mainlogo').animate({ width: '-=650px', background-position-x: '0px', background-position-y: '0px'},2000,'linear');", 2000,);
+	$('#mainlogo').css('text-align','center');
+	setTimeout("$('#mainlogo').animate({ width: '-=650px', text-align: 'left'},2000);", 2000,);
 };
 
 $(document).ready(main);
