f777ac41d172a53e7137c6d45ec3d87ef48884e5
Kyle Wilson <bluyam@tamu.edu>
2014-11-08 21:50:44 -0600

    changed sub position 5

--- caveat/style.js
+++ caveat/style.js
@@ -1,7 +1,7 @@
 var main = function() {
 	var intro = function () {
-		$('#sub').animate({ opacity: '-=1'},1000);
-		$('#opening').animate({ width: '-=650px'},2000);
+		$('#sub').animate({ opacity: '-=1'},400);
+		$('#mainlogo').animate({ width: '-=650px'},2000);
 	}
 	setTimeout(intro, 2000);
 };
