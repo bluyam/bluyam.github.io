caf36493a0e480105814026041dc17848857877f
Kyle Wilson <bluyam@tamu.edu>
2014-11-08 21:08:53 -0600

    image animation 1

--- caveat/style.js
+++ caveat/style.js
@@ -1,5 +1,5 @@
 var main = function() {
-	setTimeout("$('#mainlogo').animate({ width: '-=650px', background-position: '0px 0px'},2000);", 2000);
+	setTimeout("$('#mainlogo').animate({ width: '-=650px', background-position-x: '0px', background-position-y: '0px'},2000);", 2000);
 };
 
 $(document).ready(main);
