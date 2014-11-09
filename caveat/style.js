fbe2b0c52b1481b4d69eb69e4862244724a20e02
Kyle Wilson <bluyam@tamu.edu>
2014-11-08 20:48:44 -0600

    general animation 4

--- caveat/style.js
+++ caveat/style.js
@@ -1,6 +1,5 @@
 var main = function() {
-	var zoomout = function() {}
-	setTimeout("$('#mainlogo').animate({ width: '-=650px', align: 'left'},2000);", 3000);
+	setTimeout("$('#mainlogo').animate({ width: '-=650px'},2000);", 3000);
 };
 
 $(document).ready(main);
