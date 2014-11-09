e6506485e28c20766c0c43b3a12245dc579bb930
Kyle Wilson <bluyam@tamu.edu>
2014-11-08 21:41:40 -0600

    correctly called intro function 4

--- caveat/style.js
+++ caveat/style.js
@@ -1,6 +1,6 @@
 var main = function() {
 	var intro = function () {
-		$('#chiv').animate({ opacity: '-=100'},2000);
+		$('#chiv').animate({ opacity: '-=1'},2000);
 		$('#mainlogo').animate({ width: '-=650px'},2000);
 	}
 	setTimeout(intro, 2000);
