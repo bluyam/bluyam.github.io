23bca5f5c14dba51989c4cebc7effa08850718ee
Kyle Wilson <bluyam@tamu.edu>
2014-11-08 20:24:15 -0600

    delay animation 2

--- caveat/style.js
+++ caveat/style.js
@@ -1,5 +1,5 @@
 var main = function() {
-	setTimeout('("#mainlogo").animate({ width: "-=250px"},2000);', 4000 );
+	setTimeout("('#mainlogo').animate({ width: '-=250px'},2000);", 4000);
 };
 
 $(document).ready(main);
