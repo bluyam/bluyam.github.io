d15d4af92c9b01ab5de80ca0cce2e44ff9bd9880
Kyle Wilson <bluyam@tamu.edu>
2014-11-08 21:01:27 -0600

    animation time change

--- caveat/style.js
+++ caveat/style.js
@@ -1,5 +1,5 @@
 var main = function() {
-	setTimeout("$('#mainlogo').animate({ width: '-=650px', class: 'left' },2000);", 2000);
+	setTimeout("$('#mainlogo').animate({ width: '-=650px'},2000);", 2000);
 };
 
 $(document).ready(main);
